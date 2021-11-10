170bc15638673fc5f99daaec5ebc9122fa34ec76
Fandi <fandi.wibowo@spesolution.com>
2021-11-10 12:08:47 +0700

    up data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,8 @@ class AuthController extends Controller
 
     public function login(Request $request) {
 
+
+        /// tess
         $validate = Validator::make($request->all(), [
             'email' => 'required',
             'password' => 'required',
