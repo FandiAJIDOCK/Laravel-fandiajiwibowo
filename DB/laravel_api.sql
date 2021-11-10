2a8e14bc9fe0df6d3810627c9898ffe3058efd44
Fandi <fandi.wibowo@spesolution.com>
2021-11-10 15:14:34 +0700

    update project

--- DB/laravel_api.sql
+++ DB/laravel_api.sql
@@ -1,112 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 10 Nov 2021 pada 04.29
--- Versi server: 10.4.19-MariaDB
--- Versi PHP: 7.4.19
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.19-MariaDB : Database - laravel_api
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`laravel_api` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
---
--- Database: `laravel_api`
---
+USE `laravel_api`;
 
--- --------------------------------------------------------
+/*Table structure for table `failed_jobs` */
 
---
--- Struktur dari tabel `failed_jobs`
---
+DROP TABLE IF EXISTS `failed_jobs`;
 
 CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+/*Data for the table `failed_jobs` */
 
---
--- Struktur dari tabel `migrations`
---
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
 
 CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data untuk tabel `migrations`
---
+/*Data for the table `migrations` */
 
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(9, '2014_10_12_000000_create_users_table', 1),
-(10, '2014_10_12_100000_create_password_resets_table', 1),
-(11, '2019_08_19_000000_create_failed_jobs_table', 1),
-(12, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+insert  into `migrations`(`id`,`migration`,`batch`) values 
+(9,'2014_10_12_000000_create_users_table',1),
+(10,'2014_10_12_100000_create_password_resets_table',1),
+(11,'2019_08_19_000000_create_failed_jobs_table',1),
+(12,'2019_12_14_000001_create_personal_access_tokens_table',1);
 
--- --------------------------------------------------------
+/*Table structure for table `password_resets` */
 
---
--- Struktur dari tabel `password_resets`
---
+DROP TABLE IF EXISTS `password_resets`;
 
 CREATE TABLE `password_resets` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+/*Data for the table `password_resets` */
 
---
--- Struktur dari tabel `personal_access_tokens`
---
+/*Table structure for table `personal_access_tokens` */
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
 
 CREATE TABLE `personal_access_tokens` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` int(10) UNSIGNED NOT NULL,
+  `tokenable_id` int(10) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_id_foreign` (`tokenable_id`),
+  CONSTRAINT `personal_access_tokens_tokenable_id_foreign` FOREIGN KEY (`tokenable_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data untuk tabel `personal_access_tokens`
---
+/*Data for the table `personal_access_tokens` */
 
-INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `created_at`, `updated_at`) VALUES
-(11, 'App\\Models\\User', 1, 'token-auth', 'c3417d43fe4b59f46ec1bf60f0d89f29d5bc34c0c9ec27c0f9903dfa5815a6fc', '[\"*\"]', NULL, '2021-11-09 01:25:57', '2021-11-09 01:25:57'),
-(12, 'App\\Models\\User', 1, 'token-auth', 'a1f893b5284819e65b0141e30d884d99eee51206a121e1433e35f19dbe3e4d26', '[\"*\"]', NULL, '2021-11-09 01:25:58', '2021-11-09 01:25:58'),
-(18, 'App\\Models\\User', 1, 'token-auth', '813a9f8f57fe750473b887f2a3ce5b48d77926a0ab3cfb60594685819a103ca1', '[\"*\"]', NULL, '2021-11-09 07:08:40', '2021-11-09 07:08:40'),
-(63, 'App\\Models\\User', 56, 'token-auth', '3f53099a2276960c66b102a6f0df211994e48234b50a7b28df2b24fb6f9ed2aa', '[\"*\"]', NULL, '2021-11-09 18:47:34', '2021-11-09 18:47:34');
+insert  into `personal_access_tokens`(`id`,`tokenable_type`,`tokenable_id`,`name`,`token`,`abilities`,`last_used_at`,`created_at`,`updated_at`) values 
+(11,'App\\Models\\User',1,'token-auth','c3417d43fe4b59f46ec1bf60f0d89f29d5bc34c0c9ec27c0f9903dfa5815a6fc','[\"*\"]',NULL,'2021-11-09 08:25:57','2021-11-09 08:25:57'),
+(12,'App\\Models\\User',1,'token-auth','a1f893b5284819e65b0141e30d884d99eee51206a121e1433e35f19dbe3e4d26','[\"*\"]',NULL,'2021-11-09 08:25:58','2021-11-09 08:25:58'),
+(18,'App\\Models\\User',1,'token-auth','813a9f8f57fe750473b887f2a3ce5b48d77926a0ab3cfb60594685819a103ca1','[\"*\"]',NULL,'2021-11-09 14:08:40','2021-11-09 14:08:40'),
+(64,'App\\Models\\User',1,'token-auth','f80579ab684da23c510645560364012513e7bd72c368c6bfdba1124fdfae0cc7','[\"*\"]',NULL,'2021-11-10 03:36:22','2021-11-10 03:36:22');
 
--- --------------------------------------------------------
+/*Table structure for table `users` */
 
---
--- Struktur dari tabel `users`
---
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
@@ -119,94 +112,17 @@ CREATE TABLE `users` (
   `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `registered_at` timestamp NULL DEFAULT NULL,
   `otp` int(11) DEFAULT NULL,
-  `verified` int(11) DEFAULT NULL COMMENT '0= belum verified, 1= verifdied'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `verified` int(11) DEFAULT NULL COMMENT '0= belum verified, 1= verifdied',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data untuk tabel `users`
---
+/*Data for the table `users` */
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `status`, `remember_token`, `created_at`, `updated_at`, `role`, `avatar`, `registered_at`, `otp`, `verified`) VALUES
-(1, 'fandi', 'fndwbw688@gmail.com', NULL, '$2y$10$e1hWQzCGmplP8I1rlG52temfZ1ITXwBM4qCKUo8hKysRyPGO3CdES', 'aktif', NULL, '2021-11-08 21:36:12', '2021-11-08 21:36:12', NULL, NULL, NULL, NULL, NULL),
-(56, 'jojo', 'hasfamediapratama@gmail.com', NULL, '$2y$10$LpFMBOz8EaYxJkklS6iFT.vxwGAelvbXZ/eMfmzjffJnFz8ryC4Vq', 'aktif', NULL, '2021-11-09 18:47:34', '2021-11-09 18:47:34', 'user', NULL, '2021-11-09 18:47:34', NULL, NULL);
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`status`,`remember_token`,`created_at`,`updated_at`,`role`,`avatar`,`registered_at`,`otp`,`verified`) values 
+(1,'fandi','fndwbw688@gmail.com',NULL,'$2y$10$e1hWQzCGmplP8I1rlG52temfZ1ITXwBM4qCKUo8hKysRyPGO3CdES','aktif',NULL,'2021-11-09 04:36:12','2021-11-09 04:36:12',NULL,NULL,'2021-11-09 15:01:01',234543,1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indeks untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
-
---
--- Indeks untuk tabel `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_id_foreign` (`tokenable_id`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT untuk tabel `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD CONSTRAINT `personal_access_tokens_tokenable_id_foreign` FOREIGN KEY (`tokenable_id`) REFERENCES `users` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
